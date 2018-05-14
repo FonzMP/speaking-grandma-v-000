f94788bd08567fb1775ce4dac46fa2a7a068a8ce
Alex Wilson <fonzmp@gmail.com>
2018-05-14 17:58:27 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -7,7 +7,7 @@
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif !string == string.uppercase
+  elsif !string == string.upcase
     return "HUH?! SPEAK UP, SONNY!"
   end
 end
