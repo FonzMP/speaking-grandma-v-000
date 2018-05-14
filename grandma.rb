f2a205662f73067847f32f078991d8acfcf916b6
Alex Wilson <fonzmp@gmail.com>
2018-05-14 17:50:47 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@
 # unless you shout it (type in all capitals).
 
 def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
+  if phrase = "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN"
   end
 end
